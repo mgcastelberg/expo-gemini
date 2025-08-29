9e4cf923f119633a0324865e814acd81d0944060
ManuxDark <mgcastelberg@hotmail.com>
2025-08-29 00:36:04 -0600

    Zustand - Crear mensaje

--- actions/gemini/basic-prompt.action.ts
+++ actions/gemini/basic-prompt.action.ts
@@ -1,6 +1,6 @@
 import geminiApi from "../gemini.api";
 
-export const basicPromptAction = async (prompt: string) => {
+export const getBasicPrompt = async (prompt: string): Promise<string> => {
     try {
         const response = await geminiApi.post('/basic-prompt', { prompt }, { responseType: 'text' });
         console.log({data: response.data});
